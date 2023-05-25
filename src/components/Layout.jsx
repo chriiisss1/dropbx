5d651c1b1f0802957278fd12ac2155d0c48f0382
chris39924 <christiandolores552@gmail.com>
2023-05-25 14:25:07 -0500

    noseqponer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <Landing />
     </>
   )
 }
