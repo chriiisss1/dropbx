2951fb0efd34e526e36aae9a0dc5c46d219e53fd
chris39924 <christiandolores552@gmail.com>
2023-05-25 14:39:08 -0500

    aaaa

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+
 import Navbar from './Navbar'
+
+import Landing from '../pages/Landing'
 
 const Layout = () => {
   return (
-    <>
+    <div>
       <Navbar />
       <Landing />
-    </>
+    </div>
   )
 }
 
