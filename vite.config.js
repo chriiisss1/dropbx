174ff690c4917797d6363455622fae406d8628ca
chris39924 <130947027+chris39924@users.noreply.github.com>
2023-06-01 08:20:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dropbx/'
+  base: '/dropbx/' // Eliminar al no usar github pages (causa error)
 })
